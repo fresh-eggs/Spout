754309b88624fb6a7dc50ac994093dff7551a501
vincelasal <zack.netsec@gmail.com>
2015-01-26 19:06:33 -0600

    Changed ThreadList location
    The thread list is now one instance inside the server.

--- Source/ServerThread.java
+++ Source/ServerThread.java
@@ -1,11 +1,6 @@
-
-
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
@@ -14,7 +9,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.security.InvalidKeyException;
 import java.security.KeyFactory;
-import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.PublicKey;
 import java.security.SecureRandom;
@@ -31,7 +25,6 @@ import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.Mac;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.apache.commons.codec.binary.Base64;
@@ -46,12 +39,10 @@ public class ServerThread implements Runnable
 	protected Socket threadSock;
 	protected Thread listener;
 	private Random rnd = new Random();
-	
-	//List of all server threads active.
-	protected static Vector<ServerThread> serverThreadList = new Vector<ServerThread>();//its static so the same one is used for all.
-	
-	//RFC Compliant HMAC variables.
 	private static final String HMAC_SHA1_ALGORITHM = "HmacSHA1";
+	
+	//List of threads
+	Vector<ServerThread> serverThreadList = TCPServer.getThreadList();
 	
 	//Used to suspend all regular chat functions while there is a client connecting as to free up tunnels for RSA exchange.
 	private boolean suspendAll;
@@ -74,12 +65,15 @@ public class ServerThread implements Runnable
 	 */
 	public void start()
 	{
-		try {
+		try 
+		{
 			inFromClient = new ObjectInputStream(threadSock.getInputStream());
 			outToClient = new ObjectOutputStream(threadSock.getOutputStream());
-		    }catch (IOException e) {
-		    	//System.exit(0);
-			e.printStackTrace();}
+		}
+		catch (IOException e) 
+		{
+			e.printStackTrace();
+		}
 		
 	    listener = new Thread(this);
 		listener.start();
@@ -93,142 +87,133 @@ public class ServerThread implements Runnable
 	public void run() 
 	{
 
-		
+
 		try {
-			
+
 			//NEW USER
 			//Gather up the username hash
 			String usernameHash;
-			while((usernameHash = (String) inFromClient.readObject()) == null){
-				usernameHash = (String) inFromClient.readObject();
-			}
+			usernameHash = (String) inFromClient.readObject();
 			System.out.println(usernameHash);
-			
+
 			//In here we are checking our hash database to see if 
 			//the client is new or returning.
 			boolean isReturning = false;
-			File dir = new File("/home/nick/");
+			File dir = new File("C:/Users/Public/Documents/srv/");
 			File[] directoryListing = dir.listFiles();
 			String usernameHashFile = new String(usernameHash+".txt");
-			
+
+			//Try to find a hash that matches=
 			if (directoryListing != null) 
 			{
 				for (File child : directoryListing) 
 				{
 					String filename = child.getName();
-					if(usernameHashFile.equals(filename)){
+					if(usernameHashFile.equals(filename))
+					{
 						isReturning = true;
 					}
 				}
 			} 
-			
 
-			
+
+
 			if(isReturning)
 			{
-				
 				System.out.println("we found a returing user");
 				System.out.println("begin reuturning user process");
-			    //OLD USER
-				Path path = Paths.get("/home/nick/"+usernameHash+".txt");
-				
+				//OLD USER
+				Path path = Paths.get("C:\\Users\\Public\\Documents\\srv\\"+usernameHash+".txt");
+
 				//Create public key from encoded bytes,
 				byte[] encodedPublic = Files.readAllBytes(path);
 				KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-			    EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(encodedPublic);
-			    PublicKey publicKeyForStorage = keyFactory.generatePublic(publicKeySpec);
-			    
+				EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(encodedPublic);
+				PublicKey publicKeyForStorage = keyFactory.generatePublic(publicKeySpec);
+
 				//AUTHENTICATION
-			    //Create the random mess used to sign.
-			    SecureRandom rnd = new SecureRandom();
+				//Create the random mess used to sign.
+				SecureRandom rnd = new SecureRandom();
 				byte[] randomBytes = new byte[16];
 				rnd.nextBytes(randomBytes);
-				String messToClient = new String(Hex.encodeHex(randomBytes));
 				
-				//Create the digest
-				MessageDigest sha1 = MessageDigest.getInstance("SHA-1");
-				byte[] temp = sha1.digest(messToClient.getBytes());
-				String srvDigest = new String(temp);		
+				//the temp secret we will share with the client
+				String HMACsecret = new String(Hex.encodeHex(randomBytes));
 				
-				System.out.println("What was sent to client: " +srvDigest);
-				outToClient.writeObject(srvDigest);	
-				
-			    //Wait for the signature.
-			    String encodedEncryptedDigest = (String) inFromClient.readObject();
+				//Encrypt and encode it then send it.
+				String encryptedHMACsecret =  EncryptRSA(HMACsecret, publicKeyForStorage); 
+				outToClient.writeObject(encryptedHMACsecret);
 
-			    
-			    System.out.println("This is the encrypted encoded string we get: "+encodedEncryptedDigest);
-			    byte[] encryptedDigest = Base64.decodeBase64(encodedEncryptedDigest.getBytes());
-				
-			    //decrypt digest with our public key we use for the digital signature. 
-				Cipher cipher = Cipher.getInstance("RSA");
-				cipher.init(Cipher.DECRYPT_MODE, publicKeyForStorage);
-				String digest = new String((cipher.doFinal(encryptedDigest)));
-				
-				System.out.println("This is what we got from client:" +digest);
-				System.out.println("This is the hash of what we sent:" +srvDigest);
-				
-				if((digest.equals(srvDigest)) == false)
-				{
-					try {threadSock.close();
-					}catch (IOException e) {
-						e.printStackTrace();
-					}
-					serverThreadList.removeElement(this);
-				}
-				
-				publicKeySwap(publicKeyForStorage);
+				sharedSecretSwapReturning(publicKeyForStorage, HMACsecret);
 			}
 			else
 			{
-				System.out.println("Start some new client shit");
+				System.out.println("Start some new client junk");
 
-				//New user, go ahead and gather up the encoded pubkey then store it with
-				//the usernamehash as the title to be used for authentication.
+				//Generate a temporary PIN to be used during the authentication of a new client.
 				int PIN = 100000 + rnd.nextInt(900000);
 				String PINstring = Integer.toString(PIN);
 				System.out.println("This is the PIN for the new client:" +PIN);
 
-				
-				//Receive the HMAC
-				String userHMAC = (String) inFromClient.readObject();
+				//Receive new user's HMAC for the public key they entended to send.
+				String clientHMAC = (String) inFromClient.readObject();
 				
 				//Receive new user's public key 
 				String encodedPublicKey = (String) inFromClient.readObject();
+
+				//Calculate an HMAC on the encoded public key bytes to see if we have a match. 
+				//If we do than we can be sure we received what the client intended to send.
+				String HMAC = calculateHMAC(encodedPublicKey, PINstring);
 				
-				//Calcualte own HMAC
-				String srvHMAC = calculateHMAC(encodedPublicKey, PINstring);
-				
-				if(!(srvHMAC.equals(userHMAC)))//check to see if pin's match.
+				//Compare the one we calculated with the one the client sent.
+				//if they are not the same, kick the client off and remove them from list.
+				if(!HMAC.equals(clientHMAC))
 				{
-					System.out.println("YOU ARE NOT WHO YOU SAY YOU ARE");
-					threadSock.close();
+					try 
+					{
+						threadSock.close();
+					}
+					catch (IOException e) 
+					{
+						e.printStackTrace();
+					}
+					serverThreadList.removeElement(this);
+					
+					System.out.println("The HMAC we calculated did not match the one the client sent. Someone is being the NSA...");
 				}
 				else
 				{
-					//Decode public key so we can make an object with it.
+					//if they do match, continue on with the storage.
+					System.out.println("The HMAC we calculated did match the one the client sent. MEOW");
+					
+					//Decode the public key bytes
 					byte[] decodedPublicKey = Base64.decodeBase64(encodedPublicKey.getBytes());
 					
-					//Create public key from encoded bytes,
+					//Create public key from decoded bytes,
 					KeyFactory keyFactory = KeyFactory.getInstance("RSA");
-				    EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(decodedPublicKey);
-				    PublicKey publicKeyForStorage = keyFactory.generatePublic(publicKeySpec);
-				
-				    //Store the users public key/usernamehash.
-				    File directory = new File("/home/nick/");
-				    directory.mkdir();
-					FileOutputStream keyfos2 = new FileOutputStream("/home/nick/"+usernameHash+".txt");
+					EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(decodedPublicKey);
+					PublicKey publicKeyForStorage = keyFactory.generatePublic(publicKeySpec);
+
+					//Create a directory to store the user's public keys.
+					File directory = new File("C:/Users/Public/Documents/srv/");
+					directory.mkdir();
+					
+					//Store the bytes for the public key.
+					//This public key is now associated to that username
+					FileOutputStream keyfos2 = new FileOutputStream("C:/Users/Public/Documents/srv/"+usernameHash+".txt");
 					keyfos2.write(decodedPublicKey);
 					keyfos2.close();  
-					
-					publicKeySwap(publicKeyForStorage);
+
+					//Send over the shared secret to the client that the network shares.
+					sharedSecretSwapNewUser(publicKeyForStorage, PINstring);
 				}
+
 			}
-			
+
 		} catch (IOException e) {
 			e.printStackTrace();
 		} catch (InterruptedException e) {
-				e.printStackTrace();
+			e.printStackTrace();
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} catch (NoSuchAlgorithmException e) {
@@ -244,10 +229,11 @@ public class ServerThread implements Runnable
 		} catch (BadPaddingException e) {
 			e.printStackTrace();
 		} catch (SignatureException e) {
-			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 
 		
 		//only thing we want run() to do is spam send to all.
@@ -259,18 +245,25 @@ public class ServerThread implements Runnable
 		System.out.println("Entering main thread loop.");
 		while(clientConnected)
 		{
-			try {
+			try 
+			{
 				sendToAll((String) inFromClient.readObject());
-			} catch (IOException e1) 
+			} 
+			catch (IOException e1) 
 			{
 				e1.printStackTrace();
-				try {threadSock.close();
-				}catch (IOException e) {
+				try 
+				{
+					threadSock.close();
+				}
+				catch (IOException e) 
+				{
 					e.printStackTrace();
 				}
 				clientConnected = false;
 				serverThreadList.removeElement(this);
-			} catch (ClassNotFoundException e) {
+			} 
+			catch (ClassNotFoundException e) {
 				e.printStackTrace();
 			} 
 		}
@@ -292,16 +285,82 @@ public class ServerThread implements Runnable
 				while(enumerator.hasMoreElements())
 				{
 					ServerThread srvThread = (ServerThread)enumerator.nextElement();
-					try{
+					try
+					{
 						TCPServer.getGUI().getChatDisplay().append(encryptedString + "\n");
 						srvThread.outToClient.writeObject(encryptedString);
-					   }catch (IOException e1) {
+					}
+					catch (IOException e1) 
+					{
 						e1.printStackTrace();
 					}
 				}
 			}	
 		}
 	}
+	
+	
+
+	synchronized private void sharedSecretSwapNewUser(PublicKey value, String HMACsecret) throws IOException, 
+	InterruptedException, ClassNotFoundException, NoSuchAlgorithmException, InvalidKeySpecException, 
+	NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, SignatureException
+	{
+		String encryptedSharedBytes = null;
+		try 
+		{
+			encryptedSharedBytes = EncryptRSA(sharedBitString, value);
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		
+		String HMAC = calculateHMAC(encryptedSharedBytes, HMACsecret);
+		
+		outToClient.writeObject(HMAC);
+		outToClient.writeObject(encryptedSharedBytes);
+		System.out.println("Sent Encrypted private secret to client");
+	}
+	
+	
+	
+	synchronized private void sharedSecretSwapReturning(PublicKey pubKey, String HMACsecret) throws IOException, 
+	InterruptedException, ClassNotFoundException, NoSuchAlgorithmException, InvalidKeySpecException, 
+	NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, SignatureException
+	{
+		String encryptedSharedBytes = null;
+		try 
+		{
+			encryptedSharedBytes = EncryptRSA(sharedBitString, pubKey);
+		} 
+		catch (Exception e) 
+		{
+			e.printStackTrace();
+		}
+		
+		String HMAC = calculateHMAC(encryptedSharedBytes, HMACsecret);
+		
+		outToClient.writeObject(HMAC);
+		outToClient.writeObject(encryptedSharedBytes);
+		System.out.println("Sent Encrypted private secret to client");
+	}
+	
+	
+	
+	private static String EncryptRSA(String plainText, PublicKey pubKey)  throws Exception
+	{
+		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+		cipher.init(Cipher.ENCRYPT_MODE, pubKey);
+
+		String encodedEncryptedString = new String(Base64.encodeBase64(cipher.doFinal(plainText.getBytes())));
+		return encodedEncryptedString;
+	}
+
+						
+	protected ObjectOutputStream getOutToClient() {
+		return outToClient;
+	}
+	
 	
 	
 	//=======================================
@@ -330,51 +389,13 @@ public class ServerThread implements Runnable
 			// base64-encode the hmac
 			result = new String(Base64.encodeBase64(rawHmac));
 
-		} catch (Exception e) {
+		} 
+		catch (Exception e) 
+		{
 			throw new SignatureException("Failed to generate HMAC : " + e.getMessage());
 		}
 	
 		return result;
 	}
 
-	
-	
-	synchronized private void publicKeySwap(PublicKey value) throws IOException, InterruptedException, ClassNotFoundException, NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException
-	{
-	    
-		//==============================
-		//		Create and encrypt
-		//==============================
-		
-		String encryptedSharedBytes = null;
-		try {
-			encryptedSharedBytes = EncryptRSA(sharedBitString, value);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-		outToClient.writeObject(encryptedSharedBytes);
-		
-		System.out.println("Sent Encrypted private secret to client");
-	}
-	
-	
-	private static String EncryptRSA(String plainText, PublicKey pubKey)  throws Exception
-	{
-		Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
-		cipher.init(Cipher.ENCRYPT_MODE, pubKey);
-
-		String encodedEncryptedString = new String(Base64.encodeBase64(cipher.doFinal(plainText.getBytes())));
-		return encodedEncryptedString;
-	}
-
-						
-	protected ObjectOutputStream getOutToClient() {
-		return outToClient;
-	}
 }
-
-
-
-
-
