63daafebe80e7cb4bc80330142c6f9dc6a2bac18
vincelasal <zack.netsec@gmail.com>
2015-01-26 19:09:19 -0600

    Added thread list
    Added list of threads and a max.

--- Source/TCPServer.java
+++ Source/TCPServer.java
@@ -1,28 +1,28 @@
 
-
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.ArrayList;
+import java.util.Vector;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
 import java.security.NoSuchProviderException;
 import java.security.SecureRandom;
+
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
 public class TCPServer 
 {
-	private static ArrayList<ServerThread> threadList = new ArrayList<ServerThread>();//array of server threads for concurrency
-//	private String sharedString;//placeholder for the secret bits sent to each instance of server thread.
 	private static boolean bitsEntered = false;//to make loop wait for bits entered.
 	final static TheGUIServer theGui = new TheGUIServer();//the servers GUI
 	private static SecureRandom secureRnd = new SecureRandom();
+	
+	//List of all server threads active.
+	protected static Vector<ServerThread> serverThreadList = new Vector<ServerThread>();
 
-	public TCPServer(Socket socket)	{
-	}
 	
 
 	public static void main(String[] args) throws IOException, InvalidKeyException,
@@ -30,21 +30,16 @@ public class TCPServer
 			NoSuchAlgorithmException, NoSuchPaddingException, InterruptedException, NoSuchProviderException
 	{
 		final ServerSocket serverSocket = new ServerSocket(6874);
-		
-		//Create new Secure random obj in order to generate our
-		//private key. It will be a 128 bit AES key. The secure
-		//random class seeds from environment sources of entropy
-		//along with a wide selection of results making it a good
-		//source of pseudo random junk.
 		final int AES_KEY_SIZE = 16;
+		
 		
 		byte[] temp = new byte[AES_KEY_SIZE];
 		secureRnd.nextBytes(temp);
 		String sharedString = new String(temp);
 		theGui.setChatDisplay(sharedString + "\n");
 		theGui.setChatDisplay(sharedString.length() + "\n");
-		
 		theGui.setChatDisplay("Waiting for clients......" + "\n");
+		
 		
 		//In order to start our nested while, accept more is set to false but has the check
 		//before every pass of the inner acceptMore in order to see if the size is ok for 
@@ -52,7 +47,7 @@ public class TCPServer
 		boolean acceptMore = false;	
 		while(!acceptMore)
 		{
-			if(ServerThread.serverThreadList.size() < 15)
+			if(serverThreadList.size() < 15)
 				acceptMore = true;
 			
 			while(acceptMore)
@@ -60,7 +55,7 @@ public class TCPServer
 				Socket client = serverSocket.accept();//wait until tcp handshake happens on port 6874
 				theGui.setChatDisplay("Client Connected: " + client.getInetAddress() + "\n");
 				
-				if(ServerThread.serverThreadList.size() >= 15)
+				if(serverThreadList.size() >= 15)
 				{
 					theGui.setChatDisplay("Client: "+client.getInetAddress()+
 							" was rejected. Too many clients already connected." + "\n");
@@ -75,6 +70,11 @@ public class TCPServer
 				}
 			}
 		}
+	}
+	
+	protected static Vector<ServerThread> getThreadList()
+	{
+		return serverThreadList;
 	}
 	
 	protected static boolean areBitsEntered()
